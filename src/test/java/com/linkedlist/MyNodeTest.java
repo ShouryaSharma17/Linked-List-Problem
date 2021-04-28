7b71800b0f5c6d6f95d534518a4c5d37eb97dbe6
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-04-28 20:40:56 +0530

    Created Linked List By Appending

--- src/test/java/com/linkedlist/MyNodeTest.java
+++ src/test/java/com/linkedlist/MyNodeTest.java
@@ -18,14 +18,25 @@ public class MyNodeTest {
     // Test to add elements at front
     @Test
     public void givenInput_ToLinkedList_ShouldAddToFront(){
-        MyNode<Integer> firstNode = new MyNode(56);
+        MyNode<Integer> firstNode = new MyNode(70);
         MyNode<Integer> secondNode = new MyNode(30);
-        MyNode<Integer> thirdNode = new MyNode(70);
+        MyNode<Integer> thirdNode = new MyNode(56);
         MyLinkedList myLinkedList=new MyLinkedList();
         myLinkedList.add(firstNode);
         myLinkedList.add(secondNode);
         myLinkedList.add(thirdNode);
         Assertions.assertTrue(myLinkedList.head.equals(thirdNode) && myLinkedList.head.getNext().equals(secondNode) && myLinkedList.tail.equals(firstNode));
-
+    }
+    //Test to verify if the elements have appended
+    @Test
+    public void givenInput_ToLinkedList_ShouldAppendToElement(){
+        MyNode<Integer> firstNode = new MyNode(56);
+        MyNode<Integer> secondNode = new MyNode(30);
+        MyNode<Integer> thirdNode = new MyNode(70);
+        MyLinkedList myLinkedList=new MyLinkedList();
+        myLinkedList.add(firstNode);
+        myLinkedList.append(secondNode);
+        myLinkedList.append(thirdNode);
+        Assertions.assertTrue(myLinkedList.head.equals(firstNode) && myLinkedList.head.getNext().equals(secondNode) && myLinkedList.tail.equals(thirdNode));
     }
 }
