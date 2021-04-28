14233fe58a5fc1b75a58273955c8990b49ca9433
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-04-28 19:32:45 +0530

    Create List By Adding Elements To Front

--- src/test/java/com/linkedlist/MyNodeTest.java
+++ src/test/java/com/linkedlist/MyNodeTest.java
@@ -15,4 +15,17 @@ public class MyNodeTest {
         secondNode.setNext(thirdNode);
         Assertions.assertTrue(firstNode.getNext().equals(secondNode) && secondNode.getNext().equals(thirdNode));
     }
+    // Test to add elements at front
+    @Test
+    public void givenInput_ToLinkedList_ShouldAddToFront(){
+        MyNode<Integer> firstNode = new MyNode(56);
+        MyNode<Integer> secondNode = new MyNode(30);
+        MyNode<Integer> thirdNode = new MyNode(70);
+        MyLinkedList myLinkedList=new MyLinkedList();
+        myLinkedList.add(firstNode);
+        myLinkedList.add(secondNode);
+        myLinkedList.add(thirdNode);
+        Assertions.assertTrue(myLinkedList.head.equals(thirdNode) && myLinkedList.head.getNext().equals(secondNode) && myLinkedList.tail.equals(firstNode));
+
+    }
 }
